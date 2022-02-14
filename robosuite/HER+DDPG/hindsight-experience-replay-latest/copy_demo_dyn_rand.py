6b63c565c542e958bcdae7bf038f3d8a992c0520
Manpreet Singh <manprees@andrew.cmu.edu>
2022-02-14 14:28:20 -0500

    DMP Integrated

--- robosuite/HER+DDPG/hindsight-experience-replay-latest/copy_demo_dyn_rand.py
+++ robosuite/HER+DDPG/hindsight-experience-replay-latest/copy_demo_dyn_rand.py
@@ -33,6 +33,7 @@ from geometry_msgs.msg import Pose, Vector3
 from std_msgs.msg import String
 from scipy.spatial.transform import Rotation as R
 
+
 # pre_process the inputs
 def _preproc_inputs(obs, g):
     obs_norm = o_norm.normalize(obs)
@@ -49,21 +50,6 @@ def dyn_rand():
 	phone_orient = 0.0
 	# phone_orient = np.random.uniform(-0.05, 0.05)
 	return phone_x, phone_speed, phone_orient
-
-
-def dmp_client( start, goal, mode, phone_velocity):
-    rospy.wait_for_service('dmp_calc_path')
-    try:
-        dmp_calc_path = rospy.ServiceProxy('dmp_calc_path', dmpPath)
-        resp1 = dmp_calc_path(start,goal, mode, phone_velocity)
-        return resp1.traj
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
-def quat_to_euler( quat):
-    r_quat = R.from_quat([quat.x,quat.y,quat.z,quat.w])
-    e_angles = r_quat.as_euler('zyx', degrees=False)
-    return e_angles
-
 
 if __name__ == '__main__':
     args = get_args()
@@ -117,129 +103,45 @@ if __name__ == '__main__':
         ##to plot
         observation_x = np.zeros(11000)
         t_arr = np.linspace(0,11000,11000)
-        plan_flag= True
-        path_executed = False
         for t in range(env._max_episode_steps):
-
-            obs,reward,done,_ = env.step(action_zero)
-            obs_current = obs['observation']
-            
-            # When phone crosses this point, planner will get started
-            pre_grasp_pos = 0.50
-            proximal_tol = 0.01
-            
-            gripper_pos = obs_current[19:22] 
-            phone_pos = obs_current[12:15] 
-
-            delta = gripper_pos - action_zero[0:3]
-
-            if phone_pos[1]>pre_grasp_pos:
-                pass
-
-
-            if (phone_pos[1]<pre_grasp_pos and path_executed==False):
-
-
-                # Init DMP traj 
-                if(plan_flag):          
-                    start= action_zero[0:3]
-                    start_pose = Pose()
-
-                    start_pose.position.x = start[0]
-                    start_pose.position.y = start[1]
-                    start_pose.position.z = start[2]
-                    start_pose.orientation.x = 0
-                    start_pose.orientation.y = 0
-                    start_pose.orientation.z = 0
-                    start_pose.orientation.w = 1
-
-
-                    goal = np.zeros(3)
-                    goal[0] = phone_pos[0] - delta[0]
-                    goal[1] = phone_pos[1] - delta[1]
-                    goal[2] = phone_pos[2]        
-                    end_pose = Pose()
-                    end_pose.position.x = goal[0]
-                    end_pose.position.y = goal[1]
-                    end_pose.position.z = goal[2]
-                    end_pose.orientation.x = 0
-                    end_pose.orientation.y = 0
-                    end_pose.orientation.z = 0
-                    end_pose.orientation.w =1
-                    mode = String()
-                    mode.data = "pick"
-
-                    phone_velo = Vector3()
-                    phone_velo.x = 0
-                    phone_velo.y = obs_current[29]
-                    phone_velo.z = 0
-
-                    traj =dmp_client(start_pose, end_pose, mode, phone_velo)
-                    desired_traj = np.zeros((len(traj), 6))
-                    for i in range(len(traj)):
-                        desired_traj[i][0] =traj[i].position.x 
-                        desired_traj[i][1] =traj[i].position.y 
-                        desired_traj[i][2] =traj[i].position.z
-                        desired_traj[i][5] =quat_to_euler(traj[i].orientation)[0]
-                        desired_traj[i][4] =quat_to_euler(traj[i].orientation)[1]
-                        desired_traj[i][3] =quat_to_euler(traj[i].orientation)[2]
-                    traj_index = 0
-                    plan_flag = False
-
-                # ! -> Execute this traj
-
-                # Calculate current position in the trajectory : i^th index 
-                
-                
-                path_executed = traj_index==desired_traj.shape[0]
-                print(f"Path Executed {path_executed}")
-                if(path_executed==False):
-                    action_zero[0:3] = desired_traj[traj_index, 0:3]
-                    print(f"action_zero {action_zero}")
-                    obs,reward,done,_ = env.step(action_zero)
-                    obs_current = obs['observation'] 
-                    traj_index+=1
-
-
-
             # env.render()
             # if t%1000==0:
             #     print(t)
             # print("action_zero", action_zero)
-            # obs,reward,done,_ = env.step(action_zero)
-            # if t>=0 and t<1700 and pp_snatch==0:
-            #     action_network = np.zeros(3)
-            #     action_zero[1] += 0.0002 #motion happening here
-            #     if action_zero[1]>0.05:
-            #         action_zero[1]=0.05
-            #     if np.linalg.norm(obs_current[19]-obs_current[12])>0.002:
-            #         pos_x_dir = (obs_current[19]-obs_current[12])/np.abs(obs_current[19]-obs_current[12])
-            #         pos_x += 0.0001*pos_x_dir  #motion happening here
-            #         # print("x",obs_current[26])
-            #         # print(obs_current[27])
-            #     action_zero[0] = pos_x
-            #     # ipdb.set_trace()  
-            #     print(action_zero)
-            #     obs,reward,done,_ = env.step(action_zero)
-            #     obs_current = obs['observation'] 
-            #     # print("Stage 1 ",pp)
-            # ### Calculate variables to pick ###
-            # elif t>=1700 and t<1800 and pp_snatch==0:
-            #     action_network = np.zeros(3)
-            #     obs,reward,done,_ = env.step(action_zero) 
-            #     obs_current = obs['observation']
-            #     vel_iPhone = (obs_current[13] - obs_last[13])
-            #     steps_to_reach = ((0.0 - obs_current[13])/vel_iPhone)
-            #     vel_iPhone_rt = (obs_current[13] - obs_last[13])/(0.002) #rt ==> real_time
-            #     # print("Stage 2 ",pp)
-            # ### calculate target velocity ###
-            # elif t == 1800 and pp_snatch==0:
-            #     action_network = np.zeros(3)
-            #     vel_z = (obs_current[21] - 0.885)/int(steps_to_reach)
-            #     vel_y = (obs_current[20] - 0.0)/int(steps_to_reach)
-            #     # print("Stage 3 ",pp)
+            obs,reward,done,_ = env.step(action_zero)
+            if t>=0 and t<1700 and pp_snatch==0:
+                action_network = np.zeros(3)
+                action_zero[1] += 0.0002 #motion happening here
+                if action_zero[1]>0.05:
+                    action_zero[1]=0.05
+                if np.linalg.norm(obs_current[19]-obs_current[12])>0.002:
+                    pos_x_dir = (obs_current[19]-obs_current[12])/np.abs(obs_current[19]-obs_current[12])
+                    pos_x += 0.0001*pos_x_dir  #motion happening here
+                    # print("x",obs_current[26])
+                    # print(obs_current[27])
+                action_zero[0] = pos_x
+                # ipdb.set_trace()  
+                print(f"action_zero: {action_zero}")
+                obs,reward,done,_ = env.step(action_zero)
+                obs_current = obs['observation'] 
+                # print("Stage 1 ",pp)
+            ### Calculate variables to pick ###
+            elif t>=1700 and t<1800 and pp_snatch==0:
+                action_network = np.zeros(3)
+                obs,reward,done,_ = env.step(action_zero) 
+                obs_current = obs['observation']
+                vel_iPhone = (obs_current[13] - obs_last[13])
+                steps_to_reach = ((0.0 - obs_current[13])/vel_iPhone)
+                vel_iPhone_rt = (obs_current[13] - obs_last[13])/(0.002) #rt ==> real_time
+                # print("Stage 2 ",pp)
+            ### calculate target velocity ###
+            elif t == 1800 and pp_snatch==0:
+                action_network = np.zeros(3)
+                vel_z = (obs_current[21] - 0.885)/int(steps_to_reach)
+                vel_y = (obs_current[20] - 0.0)/int(steps_to_reach)
+                # print("Stage 3 ",pp)
             ### Start snatch motion ###
-            elif path_executed or np.linalg.norm(obs_current[20]-obs_current[13])<0.001 or pp_snatch == 1:
+            elif np.linalg.norm(obs_current[20]-obs_current[13])<0.001 or pp_snatch == 1:
                 # print("Stage 4 ",pp)
                 if action_zero[2]>=0.763:
                     action_zero[2]=0.763
@@ -258,8 +160,8 @@ if __name__ == '__main__':
                 action_network = np.zeros(3)
                 # print("action_zero just before Stage 4", action_zero)
                 action_zero[0]+= action_network[0] #adding del_x to current_motion
-                action_zero[1]+= action_network[1] #adding del_y to motion
-                action_zero[2]+= action_network[2]  #adding del_z to motion
+                action_zero[1]+= (vel_iPhone_rt)*0.002 + action_network[1] #adding del_y to motion
+                action_zero[2]+= vel_z*10 + action_network[2]  #adding del_z to motion
                 # action_zero[3]+= action_network[3] #adding orient_x to current_motion
                 # action_zero[4]+= action_network[4] #adding orient_y to motion
                 # action_zero[5]+= action_network[5]  #adding orient_z to motion
